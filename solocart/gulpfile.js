a959f07941efe27458e283beee87a1d9ce5f2992
kanakferdous <kanakferdous@gmail.com>
2024-09-23 14:43:07 +0600

    updated the theme. gulp added and gitignore added

---
+++ solocart/gulpfile.js
@@ -0,0 +1,61 @@
+const { src, dest, watch, series, parallel } = require("gulp");
+const sass = require("sass");
+const gulpSass = require("gulp-sass")(sass);
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const uglify = require("gulp-uglify");
+const sourcemaps = require("gulp-sourcemaps");
+const rename = require("gulp-rename");
+const gulpif = require("gulp-if");
+const browserSync = require("browser-sync").create();
+
+const isProduction = process.env.NODE_ENV === "production";
+
+// Paths
+const paths = {
+  scss: "src/scss/**/*.scss", // Source SCSS files
+  js: "src/js/**/*.js", // Source JS files
+  cssOutput: "assets", // Output styles.css to the assets folder
+  jsOutput: "assets", // Output main.js to the assets folder
+};
+
+// Compile SCSS to minified CSS and output to styles.css
+function compileSCSS() {
+  return src(paths.scss)
+    .pipe(sourcemaps.init())
+    .pipe(gulpSass().on("error", gulpSass.logError))
+    .pipe(
+      postcss([
+        autoprefixer({
+          overrideBrowserslist: ["last 2 versions"],
+          cascade: false,
+        }),
+        cssnano(), // Minifies the CSS
+      ])
+    )
+    .pipe(rename({ basename: "styles" })) // Output to styles.css
+    .pipe(sourcemaps.write("."))
+    .pipe(dest(paths.cssOutput))
+    .pipe(browserSync.stream());
+}
+
+// Minify JavaScript and output to main.js
+function minifyJS() {
+  return src(paths.js)
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(rename({ basename: "main" })) // Output to main.js
+    .pipe(sourcemaps.write("."))
+    .pipe(dest(paths.jsOutput))
+    .pipe(browserSync.stream());
+}
+
+// Watch for changes
+function watchFiles() {
+  watch(paths.scss, compileSCSS);
+  watch(paths.js, minifyJS);
+}
+
+// Default task
+exports.default = series(parallel(compileSCSS, minifyJS), watchFiles);
